18c2156163f195fa6e3093da0c135cd072be9a87
LifWS <46930172+LifWS@users.noreply.github.com>
2020-11-04 00:13:39 +0700

    Add files via upload

---
+++ TrackingObjectSet.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "TrackingObject.h"
+#include <vector>
+
+class TrackingObjectSet
+{
+public:
+	vector<TrackingObject> objs;
+	const int TRACKER_RADIUS = 100;
+	const int BORDER_RADIUS = 10;
+	TrackingObjectSet() {
+
+	}
+
+
+
+
+};
+
